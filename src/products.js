174b49fb0241ee040a3909e13f0d2490fb4ca7ac
Aviv Hadar <aviv.hadar@devalore.com>
2020-02-21 19:34:12 +0100

    Change products names

--- src/products.js
+++ src/products.js
@@ -1,10 +1,10 @@
 const products = [
   {
-    value: 'Dipoxy 900',
+    value: 'Dipoxy 900....(60%)',
     mixRatio: 0.6,
   },
   {
-    value: 'Dipoxy 55',
+    value: 'Dipoxy 55.......(20%)',
     mixRatio: 0.2,
   }
 ];
