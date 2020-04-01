877c0f38f680f87e443889da6c885af56b68a314
Aviv Hadar <aviv.hadar@devalore.com>
2020-04-01 12:07:27 +0200

    Update products

--- src/products.js
+++ src/products.js
@@ -1,11 +1,15 @@
 const products = [
   {
-    value: 'Dipoxy 900....(60%)',
-    mixRatio: 0.6,
+    value: 'Dipoxy 2K 700 (50%)',
+    mixRatio: 0.5,
   },
   {
-    value: 'Dipoxy 55.......(20%)',
-    mixRatio: 0.2,
+    value: 'Poly Pol PS 230 (0.7%)',
+    mixRatio: 0.07,
+  },
+  {
+    value: 'Resion EP101 (60%)',
+    mixRatio: 0.6,
   }
 ];
 
