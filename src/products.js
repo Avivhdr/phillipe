25f30ac99fc3d5517b34d4910e6b40c9837a5e03
Aviv Hadar <avivhdr@gmail.com>
2020-09-23 10:22:55 +0200

    Add product Resion Slow

--- src/products.js
+++ src/products.js
@@ -5,11 +5,15 @@ const products = [
   },
   {
     value: 'Poly Pol PS 230 (0.7%)',
-    mixRatio: 0.07,
+    mixRatio: 0.007,
   },
   {
     value: 'Resion EP101 (60%)',
     mixRatio: 0.6,
+  },
+  {
+    value: 'Resion Slow (43%)',
+    mixRatio: 0.43,
   }
 ];
 
